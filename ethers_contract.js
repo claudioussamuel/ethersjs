fccab013a83bfbf5a473c5854e97956f9c20ca93
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2024-12-25 01:16:21 +0000

    add code new code

--- ethers_contract.js
+++ ethers_contract.js
@@ -5,3 +5,43 @@ const sender_pvt_key =
  
 let provider = ethers.getDefaultProvider("sepolia");
 let sender_wallet = new ethers.Wallet(sender_pvt_key, provider);
+ 
+const contract_address = "0xb663c3ef12b014df88dbc9b0a7e68a1d964a53fc";
+const contract_ABI = [
+ { inputs: [], stateMutability: "nonpayable", type: "constructor" },
+ {
+   inputs: [],
+   name: "decrement",
+   outputs: [],
+   stateMutability: "nonpayable",
+   type: "function",
+ },
+ {
+   inputs: [],
+   name: "increment",
+   outputs: [],
+   stateMutability: "nonpayable",
+   type: "function",
+ },
+ {
+   inputs: [],
+   name: "variable",
+   outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+   stateMutability: "view",
+   type: "function",
+ },
+];
+ 
+const contract = new ethers.Contract(contract_address, contract_ABI, provider);
+ 
+async function interact() {
+ const value = await contract.variable();
+ console.log("Previous value:", value);
+ const contractWithSigner = contract.connect(sender_wallet);
+ const tx = await contractWithSigner.decrement();
+ await tx.wait();
+ console.log("Tx Hash: ", tx.hash);
+ const newValue = await contract.variable();
+ console.log("Current Value: ", newValue);
+}
+interact();
